e4c609786f6cf43a3a9b92d82039c7a1777c0ff3
scifilens <laughinglioness@gmail.com>
2012-12-24 00:48:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spiral::Application.config.secret_token = '102afa5a395b97ce95c0a8e500bd2e79090322912e13ab54ed40f3e8948c4fdaff2fa8b3715d923459b43be35cee81053705f696015ab8e5f93b5593464e6e54'
